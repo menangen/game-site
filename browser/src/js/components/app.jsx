450830d8d6799c7f0a0ad56a8775325b8ad39eaa
Andrey Menangen <menangen@gmail.com>
2019-07-31 22:35:07 +0300

    Buttons

--- browser/src/js/components/app.jsx
+++ browser/src/js/components/app.jsx
@@ -1,7 +1,14 @@
 import { h, app } from "hyperapp"
 
+const node = document.getElementsByTagName("app")[0]
+
 export default app({
     init: 0,
-    view: state => <h1>Hyperapp</h1>,
-    node: document.getElementsByTagName("app")[0]
+    view: state =>
+        <app>
+            <h1>Hyperapp: { state }</h1>
+            <button onClick={ state => state + 1 } >+</button>
+            <button onClick={ state => state - 1 } >-</button>
+        </app>,
+    node
 })
